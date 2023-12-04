4912b4d23bfe5a058c9ae1720cd8457a1646e608
Ania Oleksy <a.oleksy.m@gmail.com>
2023-12-04 17:48:38 +0100

    open json

--- bookstore.py
+++ bookstore.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import json
 
 bookstore = Flask(__name__)
 
-@bookstore.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+with open('books.json') as json_data:
+    result = []
+    data = json.load(json_data)
+    for book in data['books']:
+
 
 
 if __name__ == '__main__':
